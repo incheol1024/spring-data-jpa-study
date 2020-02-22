78eb7e6819f8d65e8bb22b8b35c12950af81f996
incheol1024 <yaho1024@naver.com>
2020-02-22 14:31:24 +0900

    first commit

---
+++ src/main/java/com/data/jpa/SpringDataJpaStudyApplication.java
@@ -0,0 +1,13 @@
+package com.data.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaStudyApplication.class, args);
+    }
+
+}
